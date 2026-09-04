9df0f7d9aa27117390c021a9e34fe8d42bdec55b
neo-dev <sei133dirsoa@gmail.com>
2026-09-04 07:30:27 -0300

    add few bits of the quad exercise.

--- prog2/tasks/quad.cpp
+++ prog2/tasks/quad.cpp
@@ -6,8 +6,10 @@
 
 #include <cmath> // biblioteca de funções matemáticas do C, que pode ser usada no C++
 #include <iostream> // biblioteca de fluxos (tipo importante de objeto que usa operador >> <<) que lida com entrada e saída de dados
+#include <sstream> // objeto mais genérico de fluxo, permite um parsing mais avançado
+#include <string> // classe da biblioteca padrão que gerencia um vetor dinâmico de caracteres.
 
-using namespace std; // limita o namespace global para o da biblioteca padrão, dá problemas com libs externas
+std::string lower (const std::string &);
 
 int
 main (void)
@@ -15,19 +17,46 @@ main (void)
   double a{}, b{}, c{};
   double d{}, x1{}, x2{};
 
-  // declaração moderna de variáveis, inicializadas de forma segura, ao invés do modelo c antigo que continha lixo.
+  std::string user_input;
 
-  cout << "a, b, c? ";
-  cin >> a >> b >> c;
+  // declaração moderna de variáveis, inicializadas de forma segura, ao
+  // invés do modelo c antigo que continha lixo.
 
-  d = b * b - 4 * a * c;
+  while (1)
+    {
+      user_input.clear (); // truca o buffer dinâmico para permitir o reset
+                           // do buffer de entrada do usuário, em C, seria
+                           // user_input[0] = '\0';
 
-  x1 = (-b + sqrt (d)) / (2 * a);
-  x2 = (-b - sqrt (d)) / (2 * a);
+      std::cout << "a, b, c? ";
 
-  cout << "\n\tdelta = " << d << "\n\tx1= " << x1 << "\n\tx2= " << x2 << endl;
+      std::getline (std::cin, user_input); // leio até \n do std::cin, fluxo de
+                                           // entrada em CLI do programa.
+
+      if (lower (user_input) == "Sair")
+        break;
+
+      d = b * b - 4 * a * c;
+
+      x1 = (-b + sqrt (d)) / (2 * a);
+      x2 = (-b - sqrt (d)) / (2 * a);
+
+      std::cout << "\n\tdelta = " << d << "\n\tx1= " << x1 << "\n\tx2= " << x2
+                << std::endl;
+    }
 
   return 0;
+}
+
+std::string
+lower (const std::string &ref)
+{
+  std::string prod;
+
+  for (char c : ref)
+    prod += std::tolower ((char) c);
+
+  return prod;
 }
 
 /*
@@ -36,6 +65,6 @@ main (void)
  * - Testes do delta;
  * - Validar a resposta do usuário;
  * - Criar loop de eventos do programa
- *   
+ *
  *   A tarefa é criar um programa mais robusto e com mais features
  */
