62f8fb890bbd59075106cd6ddc51c7de90089038
neo-dev <sei133dirsoa@gmail.com>
2026-09-25 13:46:24 -0300

    redo integral exercise and remove hanoi project

--- prog2/tasks/in_home/integral.cpp
+++ prog2/tasks/in_home/integral.cpp
@@ -1,5 +1,3 @@
-#include <cmath>
-#include <cstdint>
 #include <iomanip>
 #include <iostream>
 
@@ -14,39 +12,13 @@ void setconsole(void)
 }
 #endif
 
-#if defined(_MSC_VER)
-#define FORCE_INLINE __forceinline
-#else
-#define FORCE_INLINE __attribute__((always_inline)) inline
-#endif
-
-// Sem std::pow para permitir que o compilador use instruções SIMD/FMA de hardware
-FORCE_INLINE double f(double t)
+inline double f(double t)
 {
-    return (3.0 * t * t) + (4.0 * t) - 5.0;
+    return t * (t * (t + 2.0) - 5.0);
 }
 
-double calcular_riemann(double a, double b, uint64_t steps)
-{
-    const double base = (b - a) / steps;
-    double global_sum = 0.0;
-
-// Paralelismo OpenMP para distribuição de carga entre os núcleos da CPU
-#pragma omp parallel reduction(+ : global_sum)
-    {
-        double local_sum = 0.0;
-
-#pragma omp for nowait
-        for (int64_t i = 0; i < static_cast<int64_t>(steps); i++)
-        {
-            double t = a + (static_cast<double>(i) + 0.5) * base;
-            local_sum += f(t);
-        }
-        global_sum += local_sum;
-    }
-
-    return global_sum * base;
-}
+// inline força o compilador à inserir o código da função diretamente na função principal, ao invés
+// de fazer a instruçao de chamada para o código.
 
 int main(void)
 {
@@ -54,14 +26,13 @@ int main(void)
     setconsole();
 #endif
 
-    double a{1.0}, b{5.0};
-    uint64_t passos = 10e10; // 1 bilhão de iterações
+    double a{1.0};
+    double b{5.0};
+
+    double resultado{f(b) - f(a)};
 
     std::cout << std::fixed << std::setprecision(10);
-    std::cout << "Calculando integral com " << passos << " iterações...\n";
-
-    double resultado = calcular_riemann(a, b, passos);
-
+    std::cout << "Calculando integral via solucao analitica...\n";
     std::cout << "Resultado: " << resultado << " m\n";
 
     return 0;
